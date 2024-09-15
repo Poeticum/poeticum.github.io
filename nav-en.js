8413487d258d2b82d72aaaae3e3b5ee1a8bb21b5
Poeticum <165190204+Poeticum@users.noreply.github.com>
2024-09-15 13:37:19 -0600

    Edit

--- nav-en.js
+++ nav-en.js
@@ -16,16 +16,16 @@ Prose&nbsp;poetry&nbsp;by Roberto&nbsp;Q.&nbsp;S.
   <i class="navbtn" tabindex="1"></i>
   Poems
    <ul class="left">
-    <li><a href="locus-errans-en"><i>Locus Errans</i></a></li>
     <li><a href="aedis-aestusque-en"><i>Aedis Aestusque</i></a></li>
+    <li><a href="locus-errans-en"><i>Locus Errans</i></a></li>
     <li><a href="cumbres-planetarias"><i>Planetary Summits</i></a></li>
     <li><a href="las-lianas-eternas"><i>The Eternal Vines</i></a></li>
     <li><a href="el-micelio-del-amanecer"><i>The Mycelium of Dawn</i></a></li>
-   <li><a href="gotas-igneas"><i>Igneous Drops</i></a></li>
-   <li><a href="la-caverna"><i>The Cavern</i></a></li>
-   <li><a href="lar-laminar"><i>Laminar Lar</i></a></li>
-     <hr>
-        <li><a href="humores-espirituosos-miscelaneos"><i>Miscelaneous spirituous<br>humours</i></a></li>
+    <li><a href="gotas-igneas"><i>Igneous Drops</i></a></li>
+    <li><a href="la-caverna"><i>The Cavern</i></a></li>
+    <li><a href="lar-laminar"><i>Laminar Lar</i></a></li>
+      <hr>
+    <li><a href="humores-espirituosos-miscelaneos"><i>Miscelaneous spirituous<br>humours</i></a></li>
    </ul>
 </button>
  
