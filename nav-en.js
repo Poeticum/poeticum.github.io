e4dee7dfa74a29fb352b3d09c7eb838525ca4f50
Poeticum <165190204+Poeticum@users.noreply.github.com>
2024-11-03 15:26:19 -0600

    E

--- nav-en.js
+++ nav-en.js
@@ -13,28 +13,28 @@ Prose&nbsp;poetry&nbsp;by Roberto&nbsp;Q.&nbsp;S.
 <a href="/"><button class="home"></button></a>
   
 <button tabindex="1">
-  <i class="navbtn" tabindex="1"></i>
-  Poems
-   <ul class="left">
-    <li><a href="aedis-aestusque-en"><i>Aedis Aestusque</i></a></li>
-    <li><a href="locus-errans-en"><i>Locus Errans</i></a></li>
-    <li><a href="cumbres-planetarias"><i>Planetary Summits</i></a></li>
-    <li><a href="las-lianas-eternas"><i>The Eternal Vines</i></a></li>
-    <li><a href="el-micelio-del-amanecer"><i>The Mycelium of Dawn</i></a></li>
-    <li><a href="gotas-igneas"><i>Igneous Drops</i></a></li>
-    <li><a href="la-caverna"><i>The Cavern</i></a></li>
-    <li><a href="lar-laminar"><i>Laminar Lar</i></a></li>
-      <hr>
-    <li><a href="humores-espirituosos-miscelaneos"><i>Miscelaneous spirituous<br>humours</i></a></li>
-   </ul>
+<i class="navbtn" tabindex="1"></i>
+Poems
+<ul class="left">
+	<li><a href="aedis-aestusque-en"><i>Aedis Aestusque</i></a></li>
+	<li><a href="locus-errans-en"><i>Locus Errans</i></a></li>
+	<li><a class="not"><i>Planetary Summits</i></a></li>
+	<li><a class="not"><i>The Eternal Vines</i></a></li>
+	<li><a class="not"><i>The Mycelium of Dawn</i></a></li>
+	<li><a class="not"><i>Igneous Drops</i></a></li>
+	<li><a class="not"><i>The Cavern</i></a></li>
+	<li><a class="not"><i>Laminar Lar</i></a></li>
+		<hr>
+	<li><a class="not"><i>Miscelaneous spirituous<br>humours</i></a></li>
+</ul>
 </button>
  
 <button class="rt" tabindex="1">
   <i class="navbtn" tabindex="1"></i>
   Commentaries
    <ul class="right">
-        <li><a href="sobre-mi-poesia.html">On My Poetry</a></li>
-        <li><a href="sobre-la-ciencia-ficcion.html">On Science Fiction</a></li>
+        <li><a class="not">On My Poetry</a></li>
+        <li><a class="not">On Science Fiction</a></li>
    </ul>
 </button>
  
